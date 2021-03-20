d5907f7d596772c22dfac047a0ff87fb731999f9
Daniel Felix <danielfelix1995@gmail.com>
2021-03-20 14:47:01 +0530

    added comments to config file

--- config/config.php
+++ config/config.php
@@ -5,7 +5,22 @@
  */
 return [
 
+    /*
+     * Freshchat's Web Messenger Token.
+     *
+     * You can see that on Web Messenger Settings page of Freshchat Portal.
+     */
     'token'      => env('FRESHCHAT_TOKEN', null),
 
+    /*
+     * Freshchat's Web Messenger Host Value. ( it would be different based on your data region)
+     *
+     * Few examples:
+     *
+     *	https://wchat.freshchat.com
+     *	https://wchat.in.freshchat.com
+     *
+     * You can see that on Web Messenger Settings page of Freshchat Portal.
+     */
     'host'       => env('FRESHCHAT_HOST', 'https://wchat.freshchat.com'),
 ];
